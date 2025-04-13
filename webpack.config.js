f1553b2e023ed5b4b68676ffc45238c6bfb983fa
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-04-13 08:41:51 -0400

    Include `.min` in generated webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/chartjs-plugin-regression.js',
     output: {
-        filename: 'chartjs-plugin-regression.js',
+        filename: 'chartjs-plugin-regression.min.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
