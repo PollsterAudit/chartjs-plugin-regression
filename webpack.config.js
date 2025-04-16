b169d17ee02f5e32733adcc3116aed998e5bb8f4
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-04-16 05:47:02 -0400

    Optimization `keep_classnames` can be re-enabled - This was disabled so that we could call `WeightStrategies` from the browser. However, that's no longer needed since we declare it in the window.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,7 @@
-const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: ['./src/index.js', './src/weights.mjs', './src/util.mjs', './src/calculator.mjs'],
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    keep_classnames: true
-                }
-            })
-        ]
-    },
     output: {
         filename: 'chartjs-plugin-regression-trendline.min.js',
         path: path.resolve(__dirname, 'dist')
